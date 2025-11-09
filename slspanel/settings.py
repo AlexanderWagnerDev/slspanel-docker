9f6cb8ea7650cb836319f023d25fb7439c5521b8
Alexander Wagner <github@alexanderwagnerdev.com>
2025-11-09 18:23:15 +0100

    Update settings.py

--- slspanel/settings.py
+++ slspanel/settings.py
@@ -15,7 +15,7 @@ PASSWORD = os.getenv('WEB_PASSWORD', 'password')
 SRT_PUBLISH_PORT = int(os.getenv("SRT_PUBLISH_PORT", 4000))
 SRT_PLAYER_PORT = int(os.getenv("SRT_PLAYER_PORT", 4001))
 SRTLA_PUBLISH_PORT = int(os.getenv("SRTLA_PUBLISH_PORT", 5000))
-SLS_STATS_DOMAIN_IP = os.getenv("SLS_STATS_DOMAIN_IP", "localhost")
+SLS_DOMAIN_IP = os.getenv("SLS_DOMAIN_IP", "localhost")
 SLS_STATS_PORT = int(os.getenv("SLS_STATS_PORT", 8789))
 
 SLS_API_URL = os.getenv("SLS_API_URL", "http://localhost:8789")
